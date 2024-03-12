521d4a3931601d6b8466b3faa2f203b2ff82e80d
OmiCollab <163024184+OmiCollab@users.noreply.github.com>
2024-03-12 10:58:45 +0530

    Update streamlit_app.py
    Updated code

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ if ingredients_list:
         
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
     # if ingredients_string:
     #     session.sql(my_insert_stmt).collect()
     #     st.success('Your Smoothie is ordered!', icon="✅")
